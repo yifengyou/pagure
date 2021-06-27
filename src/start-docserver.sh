0f63bf8dcc1de865bf0349bd94f8702d83124f19
yifengyou <842056007@qq.com>
2021-06-27 11:45:25 +0800

    add script,modify config

---
+++ src/start-docserver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./rundocserver.py --debug --port 5001 --host 0.0.0.0 -c config
---
+++ src/start-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./runserver.py --perf-verbose --debug --port 5000 --host 0.0.0.0 -c config
