95f6639c209a484b40e628bd323161c7b265880a
yifengyou <842056007@qq.com>
2022-11-15 22:20:30 +0800

    修改扩展插件

--- dev/plugin-muti-mirror/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#ps aux|grep -E "git clone|git push"
-ps aux|grep -E 'src-openeuler|git push local'
-echo "total:"`ls -d /home/git/repositories/src-openeuler/*.git |wc -l`
-cd  /home/git/repositories/src-openeuler/
-echo "left:"`find ./ -name heads -empty |wc -l`
-find ./ -name heads -empty 
--- dev/plugin-muti-mirror/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-PAGURE_CONFIG=/etc/pagure/pagure.cfg python3 mirror_project_in.py --debug
